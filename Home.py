7d3ed9339c3754e957ecb417528bc96deb65c86f
benasmith1 <81334348+benasmith1@users.noreply.github.com>
2025-02-26 12:28:12 -0700

    typo

--- Home.py
+++ Home.py
@@ -44,7 +44,7 @@ st.markdown(
 #st.sidebar.image("Figures/houses_sidebar.jpeg", caption="California Houses")
 
 st.write("My name is Bena Smith. I am a data scientist with an M.S. in statistics. I was so excited about TikTok after applying, I built this website to showcase a few \
-        of my strengths. I performed sentiment analysis on webpages mentioning TikTok and performed time series analysis on the number of searches.\
+        of my strengths. I performed sentiment analysis on webpages mentioning TikTok.\
         I hope you enjoy my analysis!")
 
 
